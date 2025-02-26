1b7f2501139619f9fc162fb0e35988ea749a5488
Yu (Scarlett) Qiu <qiu.yu1@northeastern.edu>
2025-02-25 21:21:16 -0800

    Update q2_query.sql

--- queries/q2_query.sql
+++ queries/q2_query.sql
@@ -1,37 +1,37 @@
-with avg_per_cust_calc as(
-	select customer_id,
-	AVG(CAST(transaction_amount as float)) as avg_trans_amt
-	from accounts_transactions
-	where CAST(transaction_amount as float) > 0
-	group by customer_id
+WITH tran_90_day AS(
+SELECT consumer_id
+      , account_id
+      , merchant_name
+      , merchant_category_code
+      , transaction_date
+      , transaction_amount
+FROM accounts_transactions
+WHERE transaction_date >=DATEADD(DAY,-90,CONVERT(date,GETDATE()))
+AND transaction_amount < 0 
 ),
-stddev_per_cust_calc as(
-	select acct.customer_id,
-	apcc.avg_trans_amt,
-	SQRT((SUM((CAST(acct.transaction_amount as float) - apcc.avg_trans_amt) * (CAST(acct.transaction_amount as float) - apcc.avg_trans_amt))) / ((COUNT(*) - 1))) as stddev_amt
-	from accounts_transactions acct
-	INNER JOIN avg_per_cust_calc apcc on acct.customer_id = apcc.customer_id
-	where CAST(acct.transaction_amount AS float) > 0
-	group by acct.customer_id
-),
-query_unusual_trans as(
-	select acct.customer_id,
-	acct.account_id,
-	acct.transaction_date,
-	CAST(acct.transaction_amount as float) as transaction_amount,
-	calc.avg_trans_amt,
-	calc.stddev_amt,
-	((CAST(acct.transaction_amount AS float) - calc.avg_trans_amt) / calc.stddev_amt) as z_score,
-	CASE WHEN (ABS(acct.transaction_amount - calc.avg_trans_amt)) > (calc.stddev_amt * 3) then 1 else 0 end as flagged_trans
-	from accounts_transactions acct
-	inner join stddev_per_cust_calc calc on acct.customer_id = calc.customer_id
-	where 
-		CAST(acct.transaction_amount AS float) > 0 and calc.stddev_amt > 0
+--determine the bounds
+bounds AS(
+SELECT account_id
+      , Q1-1.5*(Q3 - Q1) AS lower
+      , Q3 + 1.5*(Q3 - Q1) AS upper
+FROM (
+SELECT account_id,
+       PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY transaction_amount) OVER (PARTITION BY account_id) AS Q1,
+       PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY transaction_amount) OVER (PARTITION BY account_id) AS Q3
+FROM tran_90_day) AS A
 )
-select * from query_unusual_trans
-where flagged_trans = 1
-order by ABS(z_score) desc;
---select * from query_unusual_trans;
---select * from stddev_per_cust_calc;
---select * from avg_per_cust_calc;
---select * from accounts_transactions at2 where customer_id = '189436ef-adf4-48ba-9f0b-d3da331c49ac'
+
+SELECT DISTINCT * FROM
+(
+SELECT DISTINCT consumer_id
+      , t.account_id, merchant_nam2
+      , merchant_category_code
+      , transaction_date
+      , transaction_amount
+      , CASE WHEN transaction_amount > upper OR transaction_amount < lower THEN 1
+        ELSE 0
+        END AS unusual
+FROM tran_90_day t
+LEFT JOIN bounds b
+ON T.account_id = B.account_id) AS B
+WHERE unusual = 1
